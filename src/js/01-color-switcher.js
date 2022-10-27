810f1cf062d94fbe8dd69c1f176d1b366b6fe88b
Берко Виталий <berko@prombudcentr.ua>
2022-10-27 15:22:00 +0300

    Added task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+function getEl(selector) {
+    return document.querySelector(selector);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+const refs ={
+    startBtn: getEl('[data-start]'),
+    stopBtn: getEl('[data-stop]'),
+    body: getEl('body'),
+};
+let timerId =null;
+
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    refs.startBtn.disabled = 1;
+    timerId =
+     setInterval(()=>{    
+    refs.body.style.backgroundColor = getRandomHexColor()}, 1000);
+
+
+};
+
+function onStopBtnClick () {
+    clearInterval(timerId);
+    refs.startBtn.disabled=0
+}
+
