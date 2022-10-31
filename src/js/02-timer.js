08df52bcb1dfff7875888610f43a0ed0d374687b
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-10-31 12:17:54 +0200

    Added task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,48 +1,41 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 function getEl(selector) {
-    return document.querySelector(selector);
-};
+  return document.querySelector(selector);
+}
 
 refs = {
-    inputCalendar: getEl('input[type="text"]'),
-    startBtn: getEl('button[type="button"]'),
-    timerDays: getEl('[data-days]'),
-    timerHours: getEl('[data-hours]'),
-    timerMinutes: getEl('[data-minutes]'),
-    timerSeconds: getEl('[data-seconds]'),
+  inputCalendar: getEl('input[type="text"]'),
+  startBtn: getEl('button[type="button"]'),
+  timerDays: getEl('[data-days]'),
+  timerHours: getEl('[data-hours]'),
+  timerMinutes: getEl('[data-minutes]'),
+  timerSeconds: getEl('[data-seconds]'),
 };
 
 let selectedTime;
-let timerId =null;
+let timerId = null;
 refs.startBtn.disabled = 1;
 
-
 const options = {
-    
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  
-  onClose(selectedDates) {
-    // console.log(selectedDates[0]);
-    // console.log(options.defaultDate);
-     currentTime = options.defaultDate.getTime();
-     selectedTime = selectedDates[0].getTime();
 
-    if(selectedTime - currentTime>0) {
-       refs.startBtn.disabled = 0;      
-       return
+  onClose(selectedDates) {
+    currentTime = options.defaultDate.getTime();
+    selectedTime = selectedDates[0].getTime();
+
+    if (selectedTime - currentTime > 0) {
+      refs.startBtn.disabled = 0;
+      return;
     }
-    window.alert('Please choose a date in the future')
+    window.alert('Please choose a date in the future');
   },
 };
 
-
-flatpickr("#datetime-picker", options); 
-
-refs.startBtn.addEventListener('click', timer.start);
+flatpickr('#datetime-picker', options);
 
 const timer = {
   start() {
@@ -50,20 +43,21 @@ const timer = {
 
     timerId = setInterval(() => {
       const currentTime = Date.now();
-      const { days, hours, minutes, seconds } = convertMs(selectedTime - currentTime);
+      const { days, hours, minutes, seconds } = convertMs(
+        selectedTime - currentTime
+      );
       refs.timerDays.textContent = days;
       refs.timerHours.textContent = hours;
       refs.timerMinutes.textContent = minutes;
       refs.timerSeconds.textContent = seconds;
+      if (selectedTime - currentTime <= 400) {
+        clearInterval(timerId);
+      }
     }, 1000);
-    
-    if (selectedTime - currentTime === 0) {
-      clearInterval(timerId);
-    }
-  }
-
+  },
 };
 
+refs.startBtn.addEventListener('click', timer.start);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -79,12 +73,13 @@ function convertMs(ms) {
   // Remaining minutes
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
-};
+}
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
