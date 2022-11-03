0eddce9c6b61baec1569db30f7c7ce8a6a72b19c
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-11-03 12:51:28 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,46 +12,52 @@ const refs = {
 
 
 let timerId = null;
-let promCounter = 0;
-let maxPromCounter = refs.amount.value;
+let position = 1;
+let maxPostion = refs.amount.value;
+let delay =refs.delay.value;
 
+refs.form.addEventListener('submit', onBtnCreatePrClick);
 
-console.log(refs.form);
-console.log(refs.delay);
-console.log(refs.step);
-console.log(refs.amount);
-
-console.log(refs.step.value);
-console.log(maxPromCounter);
+// console.log(refs.form);
+// console.log(refs.delay);
+// console.log(refs.step);
+// console.log(refs.amount);
+// console.log(refs.step.value);
+// console.log(maxPromCounter);
 
 
 function createPromise(position, delay) {
+  
+  position=+1;
+  if(position ===maxPostion) {
+    clearInterval(timerId);
+    return;
+  }
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
         // Fulfill
-        resolve();
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
         // Reject
-        reject();
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     });
   });
-
 };
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+createPromise(position, delay)
+  .then(result => {
+    Notiflix.Notify.success(result);
   })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  .catch(error => {
+    Notiflix.Notify.failure(error);
   });
 
-refs.form.addEventListener('submit', onBtnCreatePrClick);
 
 function onBtnCreatePrClick(e) {
   e.preventDefault();
   timerId = setInterval(createPromise(), refs.step.value);
+  form.reset();
 }
